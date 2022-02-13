d932f49a50e779e0435af480f91b521ae873ab5e
Khoa Huynh Tran Dang <khoa.htd@amanotes.com>
2022-02-13 23:52:08 +0700

    test cross platform

--- index.sh
+++ index.sh
@@ -4,12 +4,12 @@ download () {
 	curl -L -# --compressed -A 'https://github.com/htdangkhoa/ffmpeg-binary script' -o $2 $1
 }
 
-# echo 'darwin'
-# echo '  downloading from evermeet.cx'
-# download 'https://evermeet.cx/ffmpeg/ffmpeg-5.0.zip' darwin.zip
-# echo '  extracting'
-# mkdir -p ffmpeg/darwin
-# unzip -o -d ffmpeg/darwin -j darwin.zip ffmpeg
+echo 'darwin'
+echo '  downloading from evermeet.cx'
+download 'https://evermeet.cx/ffmpeg/ffmpeg-5.0.zip' darwin.zip
+echo '  extracting'
+mkdir -p ffmpeg/darwin
+unzip -o -d ffmpeg/darwin -j darwin.zip ffmpeg
 
 echo 'linux ia32'
 echo '  downloading from johnvansickle.com'
@@ -25,30 +25,30 @@ echo '  extracting'
 mkdir -p ffmpeg/linux-amd64
 tar -xf linux-amd64.tar.xz -C ffmpeg/linux-amd64 --strip-components=1
 
-# echo 'linux arm'
-# echo '  downloading from johnvansickle.com'
-# download 'https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-armhf-static.tar.xz' linux-arm.tar.xz
-# echo '  extracting'
-# mkdir -p ffmpeg/linux-arm
-# tar -xf linux-arm.tar.xz -C ffmpeg/linux-arm --strip-components=1
+echo 'linux arm'
+echo '  downloading from johnvansickle.com'
+download 'https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-armhf-static.tar.xz' linux-arm.tar.xz
+echo '  extracting'
+mkdir -p ffmpeg/linux-arm
+tar -xf linux-arm.tar.xz -C ffmpeg/linux-arm --strip-components=1
 
-# echo 'linux arm64'
-# echo '  downloading from johnvansickle.com'
-# download 'https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-arm64-static.tar.xz' linux-arm64.tar.xz
-# echo '  extracting'
-# mkdir -p ffmpeg/linux-arm64
-# tar -xf linux-arm64.tar.xz -C ffmpeg/linux-arm64 --strip-components=1
+echo 'linux arm64'
+echo '  downloading from johnvansickle.com'
+download 'https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-arm64-static.tar.xz' linux-arm64.tar.xz
+echo '  extracting'
+mkdir -p ffmpeg/linux-arm64
+tar -xf linux-arm64.tar.xz -C ffmpeg/linux-arm64 --strip-components=1
 
-# echo 'windows x86'
-# echo '  downloading from github.com'
-# download 'https://github.com/sudo-nautilus/FFmpeg-Builds-Win32/releases/download/latest/ffmpeg-n5.0-latest-win32-gpl-5.0.zip' windows-x86.zip
-# echo '  extracting'
-# mkdir -p ffmpeg/windows-x86
-# unzip -o -d ffmpeg/windows-x86 -j windows-x86.zip '*/bin/ffmpeg.exe'
+echo 'windows x86'
+echo '  downloading from github.com'
+download 'https://github.com/sudo-nautilus/FFmpeg-Builds-Win32/releases/download/latest/ffmpeg-n5.0-latest-win32-gpl-5.0.zip' windows-x86.zip
+echo '  extracting'
+mkdir -p ffmpeg/windows-x86
+unzip -o -d ffmpeg/windows-x86 -j windows-x86.zip '*/bin/ffmpeg.exe'
 
-# echo 'windows x64'
-# echo '  downloading from github.com'
-# download 'https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-n5.0-latest-win64-gpl-5.0.zip' windows-x64.zip
-# echo '  extracting'
-# mkdir -p ffmpeg/windows-x64
-# unzip -o -d ffmpeg/windows-x64 -j windows-x64.zip '*/bin/ffmpeg.exe'
+echo 'windows x64'
+echo '  downloading from github.com'
+download 'https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-n5.0-latest-win64-gpl-5.0.zip' windows-x64.zip
+echo '  extracting'
+mkdir -p ffmpeg/windows-x64
+unzip -o -d ffmpeg/windows-x64 -j windows-x64.zip '*/bin/ffmpeg.exe'
